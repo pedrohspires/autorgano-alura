3da14722e1834b4f5b19f7e80a9ebf247418007b
Pedro Henrique Sousa Pires <pedropires2603@gmail.com>
2023-01-20 09:44:38 -0300

    Banner

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -3,7 +3,7 @@ import './banner.css';
 const Banner = () => {
   return (
     <header className='banner'>
-      <img src="/imagens/banner.png" alt="Banner principal" />
+      <img src="/imagens/banner.jpg" alt="Banner principal" />
     </header>
   );
 }
