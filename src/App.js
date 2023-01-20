f90dc47b6ce6deffa447ef056b1afbf5f3c6a7da
Pedro Henrique Sousa Pires <pedropires2603@gmail.com>
2023-01-20 14:33:59 -0300

    Carros mockados

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Rodape from './components/Rodape';
 import Categoria from './components/Categoria';
 
 function App() {
-  const [carros, setCarros] = useState([]);
   const categorias = [
     {
       nome: "Sedan",
@@ -38,6 +37,52 @@ function App() {
       corSecundaria: "#FFF5D9"
     }
   ];
+  const carrosTemp = [
+    {
+      nome: "Elantra",
+      preco: "63.000",
+      imagem: "https://quatrorodas.abril.com.br/wp-content/uploads/2017/01/chr5632-e1579101977639.jpg?quality=70&strip=info",
+      categoria: categorias[0].nome
+    },
+    {
+      nome: "Civic",
+      preco: "107.200",
+      imagem: "https://s2.glbimg.com/8AsyIQCc2_sNbOsJXNUPQI87qYw=/0x0:1920x1080/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_cf9d035bf26b4646b105bd958f32089d/internal_photos/bs/2022/U/M/0AQABNTsW6kmItO0mGaQ/sem-titulo.jpg",
+      categoria: categorias[0].nome
+    },
+    {
+      nome: "Virtus",
+      preco: "96.000",
+      imagem: "https://img0.icarros.com/dbimg/galeriaimgmodelo/2/135409_1.webp",
+      categoria: categorias[0].nome
+    },
+    {
+      nome: "Corolla",
+      preco: "148.290",
+      imagem: "https://img2.icarros.com/dbimg/imgmodelo/4/468_16.webp",
+      categoria: categorias[0].nome
+    },
+    {
+      nome: "Golf",
+      preco: "207.291",
+      imagem: "https://cdn.motor1.com/images/mgl/XxZyk/s3/manhart-vw-golf-gti-290.jpg",
+      categoria: categorias[1].nome
+    },
+    {
+      nome: "HB20",
+      preco: "79.290",
+      imagem: "https://img1.icarros.com/dbimg/imgmodelo/4/2398_12.webp",
+      categoria: categorias[1].nome
+    },
+    {
+      nome: "Dodge Journey",
+      preco: "122.445",
+      imagem: "https://img0.icarros.com/dbimg/galeriaimgmodelo/2/126061_1.webp",
+      categoria: categorias[2].nome
+    },
+  ]
+
+  const [carros, setCarros] = useState(carrosTemp);
 
   const aoNovoCarro = (carro) => {
     setCarros([...carros, carro]);
